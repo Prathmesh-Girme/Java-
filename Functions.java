d65fde6e147ffc228e82a86e81e5eabfd62bdcec
Prathmesh Girme <106001792+Prathmesh-Girme@users.noreply.github.com>
2024-02-01 15:58:04 +0530

    Add files via upload
    Check functions concept

---
+++ Functions.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Functions{
+
+
+    public static void main(String[] args) {
+
+
+main();
+
+    }
+
+
+    static void main ()
+    {
+        Scanner num =  new Scanner(System.in);
+        System.out.println("Enter number 1 :");
+        int num1 = num.nextInt();
+        System.out.println("Enter number 2 :");
+        int num2 = num.nextInt();
+        int add = num1 + num2;
+        System.out.println("your ans is : " +  add);
+
+
+    }
+
+}
